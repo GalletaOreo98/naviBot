ecc07558c645bd32bdb32e27946ce2c42c38c8d6
GalletaOreo98 <christophergironcabrera@gmail.com>
2022-09-04 00:39:46 -0600

    UPDATE - express server

--- src/events/on-text.ts
+++ src/events/on-text.ts
@@ -2,12 +2,13 @@ import { bot } from "../index";
 import { textController } from "../controllers/text-controller";
 
 bot.on('text', ctx => {
-    textController(ctx.message.text, ctx)
+    textController(ctx.message.text, ctx);
 })
 
-/* bot.on('sticker', ctx => {    
+/* bot.on('chat_member', ctx => {    
     console.log(ctx.message);
-    ctx.replyWithSticker('CAACAgIAAxkBAAIj9GKLQek8_V-9k6zM4a4CFujDej50AAIQAAOvxlEan3fNNZ78i6ckBA')
+    ctx.replyWithSticker('CAACAgIAAxkBAAIj9GKLQek8_V-9k6zM4a4CFujDej50AAIQAAOvxlEan3fNNZ78i6ckBA');
+    ctx.reply(`Welcome! ${ctx.chatMember}`);
 }) */
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
-import { Telegraf } from 'telegraf'
-import 'dotenv/config'
+import express from 'express';
+import { Telegraf } from 'telegraf';
+import 'dotenv/config';
 
-const bot = new Telegraf(process.env.BOT_TOKEN!)
+const app = express();
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.send('Hello world!');
+})
+
+const bot = new Telegraf(process.env.BOT_TOKEN!);
 
 //Exporta la instancia del bot a los controladores que lo usaran
-export { bot }
+export { bot };
 
 /*Obtiene los controladores que se usan en el bot 
 (Incrusta todas las funcionalidades a la instancia local 'const bot')*/
-import './events/index'
+import './events/index';
 
 bot.start( ctx => {
-    ctx.reply('¡Bienvenido!')
+    ctx.reply('¡Bienvenido!');
 }) 
 
 bot.launch().then( () => {
-    console.log('Bot iniciado')
+    console.log('Bot iniciado');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port ${process.env.PORT}`);
 })
