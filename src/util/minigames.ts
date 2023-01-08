8e51671190daa536b7d1e317a09e54d121464390
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-01-08 11:25:00 -0600

    Update - Mejora codigo pptGame

--- src/util/minigames.ts
+++ src/util/minigames.ts
@@ -1,60 +1,47 @@
 import { randomBetween } from "./random";
 
-export const pptGame = (userSelection:string):string => {
+export const pptGame = (userSelection: string): string => {
 
     // piedra = 1, papel = 2, tijera = 3
-    let userSelectionRef:number = 0 
+    let userSelectionRef: number = 0;
 
     // Formateo seleccion a un numero
     if (userSelection === "piedra") {
-        userSelectionRef = 1
+        userSelectionRef = 1;
     } else {
         if (userSelection === "papel") {
-            userSelectionRef = 2
+            userSelectionRef = 2;
         } else {
             if (userSelection === "tijera" || userSelection === "tijeras") {
-                userSelectionRef = 3
+                userSelectionRef = 3;
             }
         }
     }
 
     //Genera eleccion del bot + emonji
-    const eleccionBot = randomBetween(1, 3)
-    let emoticonEleccionBot = ''
-    switch (eleccionBot) {
+    const botSelection = randomBetween(1, 3);
+    let emoticonBotSelection = ''
+    switch (botSelection) {
         case 1:
-            emoticonEleccionBot = "👊🏼"
-            break
+            emoticonBotSelection = "👊🏼";
+            break;
         case 2:
-            emoticonEleccionBot = "✋🏼"
-            break
+            emoticonBotSelection = "✋🏼";
+            break;
         case 3:
-            emoticonEleccionBot = "✌🏼"
-            break
+            emoticonBotSelection = "✌🏼";
+            break;
         default:
-            break
+            break;
     }
 
+    // p = String sobre lo que elije + emonji
+    const p = `Elijo: ${emoticonBotSelection}\n`;
     //Procesa el ganador y retorna un el mensaje
-    if (eleccionBot == userSelectionRef) {
-        return `Elijo: ${emoticonEleccionBot}\nHemos elegido lo mismo. ^^`
-    } else {
-        if (userSelectionRef == 1 && eleccionBot == 2) {
-            return `Elijo: ${emoticonEleccionBot}\nEs hora de documentar tu derrota. ^^`
-        } else {
-            if (userSelectionRef == 2 && eleccionBot == 3) {
-                return `Elijo: ${emoticonEleccionBot}\nA que he cortado tu racha, espera... ¿Siquiera tenias una?`
-            } else {
-                if (userSelectionRef == 3 && eleccionBot == 1) {
-                    return `Elijo: ${emoticonEleccionBot}\nUna dura derrota ¿no? ^^`
-                } else {
-                    if (userSelectionRef < 1 || userSelectionRef > 3) {
-                        return `Elijo: ${emoticonEleccionBot}\nEstamos jugando piedra, papel o tijeras... ¿no?`
-                    } else {
-                        return `Elijo: ${emoticonEleccionBot}\nTu ganas.`
-                    }
-                }
-            }
-        }
-    }
+    if (botSelection == userSelectionRef) return `${p}Hemos elegido lo mismo. ^^`;
+    if (userSelectionRef == 1 && botSelection == 2) return `${p}Es hora de documentar tu derrota. ^^`;
+    if (userSelectionRef == 2 && botSelection == 3) return `${p}A que he cortado tu racha, espera... ¿Siquiera tenias una?`;
+    if (userSelectionRef == 3 && botSelection == 1) return `${p}Una dura derrota ¿no? ^^`;
+    if (userSelectionRef < 1 || userSelectionRef > 3) return `${p}Estamos jugando piedra, papel o tijeras... ¿no?`;
+    return `${p}Tu ganas.`;
 }
